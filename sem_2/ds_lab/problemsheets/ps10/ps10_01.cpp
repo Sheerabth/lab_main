237bfb8cc852206bb7795f38f45a3fc63244dd18
19PW33@cs2k16.amcs.ws2k12.psgcs.edu <19PW33@cs2k16.amcs.ws2k12.psgcs.edu>
2020-03-16 16:18:52 +0530

    BST.hpp

--- sem_2/ds_lab/problemsheets/ps10/ps10_01.cpp
+++ sem_2/ds_lab/problemsheets/ps10/ps10_01.cpp
@@ -1,50 +1,13 @@
-#include<iostream>
+#include "BST.hpp"
+
 using namespace std;
-
-class BST;
-
-class Node
-{
-    int data;
-    Node *right, *left;
-    friend class BST;
-    public:
-        Node():data(0),left(NULL),right(NULL){}
-        Node(int dat):data(dat),left(NULL),right(NULL){}
-};
-
-class BST
-{
-    Node *baseRoot;
-    public:
-        BST():root(NULL){}
-        void insert(Node *&, const int&);
-        bool search(Node *&root)
-};
-
-void BST::insert(Node *& root, const int &val)
-{
-    if(!baseRoot)
-    {
-        baseRoot->data = val;
-        return;
-    }
-    else if(!root)
-    {
-        root->data = val;
-        return;
-    }
-    else if(root->data < val)
-        insert(root->right, val);
-    else
-        insert(root->left, val);
-}
 
 int main()
 {
-    Node *temp = new Node();
     BST B;
-    B.insert(temp,5);
-    B.insert(temp,)
+    B.insert(5);
+    B.insert(10);
+    B.inOrder(); 
+    B.inOrder(); 
+    B.inOrder();
 }
-
